2f2724b69429fed3634b4c1d6e4e77befb6ad8a9
Amina Alfa <Nnaalfa@yahoo.com>
2023-08-20 16:42:29 -0700

    data structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+
 def square_matrix_simple(matrix=[]):
-    return ([list(map((lambda x: x ** 2), arr)) for arr in matrix])
+    new_matrix = []
+    for col in matrix:
+        result = list(map(lambda x: x**2, col))
+        new_matrix.append(result)
+    return new_matrix
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,3 @@
+0. Write a function that computes the square value of all integers of a matrix.
+1. Write a function that replaces all occurrences of an element by another in a new list.
+2. Write a function that adds all unique integers in a list (only once for each integer).

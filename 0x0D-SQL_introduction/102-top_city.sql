db4959bce37283316db5af647c30012169aaf009
Amina Alfa <Nnaalfa@yahoo.com>
2023-10-19 13:18:36 -0700

    SQL

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,9 @@
--- displays the average temperature (Fahrenheit) by
--- city ordered by temperature (descending).
+-- displays the top 3 of cities temperature during
+-- July and August ordered by temperature (descending)
 
 SELECT city, AVG(value) AS avg_temp
 FROM temperatures
+WHERE MONTH IN (7,8)
 GROUP BY city
-ORDER BY avg_temp DESC;
+ORDER BY avg_temp DESC
+LIMIT 3;
